b886ad4cd25b4d82a203e2f1c26830f44b8e77c2
Popsabey <59439480+Popsabey@users.noreply.github.com>
2023-04-27 14:14:11 +0100

    Update Face_Dectector.py

--- Face_Dectector.py
+++ Face_Dectector.py
@@ -40,36 +40,3 @@ webcam.release()
 
 print('code completed')
 
-
-
-
-
-
-
-
-
-
-
-'''
-# convert image to greyscale, open cv's algorithm can only work well with black and white
-greyscaled_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-# detect faces
-face_coordinates = trained_faces.detectMultiScale(greyscaled_image)
-
-# Draw rectangles around faces
-# x, y,w, h are axis of the face cordinates
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 4)
-
-
-# code to show image and code to make the image wait after showing
-cv2.imshow('CLever Programmer Face Detector', img)
-cv2.waitKey()
-
-
-print("Code Completed")
-
-
-''' 
